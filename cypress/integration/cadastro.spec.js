d0756badde5970ba2e1ea3c725b636b43732b112
Dernival Liandro <liandro.sys@gmail.com>
2022-04-18 19:15:36 -0300

    Checkpoint na Automação

---
+++ cypress/integration/cadastro.spec.js
@@ -0,0 +1,12 @@
+
+
+describe('Cadastro', () => {
+    it('Usuário deve se tornar um entregador', () => {
+        cy.viewport(1920, 1080)
+        cy.visit('https://buger-eats.vercel.app/')
+
+        cy.get('a[href="/deliver"]').click();
+        cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
+
+    });
+});
--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,7 +1,7 @@
 
 
-describe('home page', () => {
-    it('app deve estar online', () => {
+describe('Home page', () => {
+    it('App deve estar online', () => {
         cy.viewport(1920, 1080)
         cy.visit('https://buger-eats.vercel.app/')
         cy.get('#page-home main h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats')
