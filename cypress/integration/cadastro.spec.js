95364baf8868fdb8fc8d6ff209fde429cabfc571
Dernival Liandro <liandro.sys@gmail.com>
2022-04-19 21:06:24 -0300

    Preenchendo dados do endereco

--- cypress/integration/cadastro.spec.js
+++ cypress/integration/cadastro.spec.js
@@ -12,12 +12,26 @@ describe('Cadastro', () => {
             nome: 'Dernival Liandro',
             cpf: '00000000041',
             email: 'teste@teste.com',
-            whatsapp: '61999999999'
+            whatsapp: '61999999999',
+            endereco: {
+                cep: '04534011',
+                rua: 'Rua Joaquim Floriano',
+                numero: '1000',
+                complemento: 'Apto 102',
+                bairro: 'Itaim Bibi',
+                cidade_uf: 'São Paulo/SP'
+            }
         }
 
         cy.get("input[name='name']").type(entregador.nome)
         cy.get("input[name='cpf']").type(entregador.cpf)
         cy.get("input[name='email']").type(entregador.email)
         cy.get("input[name='whatsapp']").type(entregador.whatsapp)
+
+        cy.get("input[name='postalcode']").type(entregador.endereco.cep)
+        cy.get("input[type=button][value='Buscar CEP']").click()
+
+        cy.get("input[name='address-number']").type(entregador.endereco.numero)
+        cy.get("input[name='address-details']").type(entregador.endereco.complemento)
     });
 });
