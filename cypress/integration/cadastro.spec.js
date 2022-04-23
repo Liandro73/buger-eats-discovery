ac3342d2163f475c67f951ea261c86da52589a0a
Dernival Liandro <liandro.sys@gmail.com>
2022-04-22 22:45:48 -0300

    Validando span de alerta

--- cypress/integration/cadastro.spec.js
+++ cypress/integration/cadastro.spec.js
@@ -50,4 +50,54 @@ describe('Cadastro', () => {
         cy.get(".swal2-container .swal2-html-container")
             .should('have.text', expectedMessage)
     });
+
+    it('CPF incorreto', () => {
+        cy.viewport(1920, 1080)
+        cy.visit('https://buger-eats.vercel.app/')
+
+        cy.get("a[href='/deliver']").click();
+        cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
+
+        var entregador = {
+            nome: 'Dernival Liandro',
+            cpf: '000000041AA',
+            email: 'teste@teste.com',
+            whatsapp: '61999999999',
+            endereco: {
+                cep: '04534011',
+                rua: 'Rua Joaquim Floriano',
+                numero: '1000',
+                complemento: 'Apto 102',
+                bairro: 'Itaim Bibi',
+                cidade_uf: 'São Paulo/SP'
+            },
+            metodo_entrega: 'Moto',
+            cnh: 'cnh-digital.jpg'
+        }
+
+        cy.get("input[name='name']").type(entregador.nome)
+        cy.get("input[name='cpf']").type(entregador.cpf)
+        cy.get("input[name='email']").type(entregador.email)
+        cy.get("input[name='whatsapp']").type(entregador.whatsapp)
+
+        cy.get("input[name='postalcode']").type(entregador.endereco.cep)
+        cy.get("input[type=button][value='Buscar CEP']").click()
+
+        cy.get("input[name='address-number']").type(entregador.endereco.numero)
+        cy.get("input[name='address-details']").type(entregador.endereco.complemento)
+
+        cy.get("input[name='address']").should('have.value', entregador.endereco.rua)
+        cy.get("input[name='district']").should('have.value', entregador.endereco.bairro)
+        cy.get("input[name='city-uf']").should('have.value', entregador.endereco.cidade_uf)
+
+        cy.contains(".delivery-method li", entregador.metodo_entrega).click()
+
+        cy.get("input[accept*='image']").attachFile("/images/" + entregador.cnh)
+
+        cy.get("form button[type='submit'").click()
+
+        cy.get(".alert-error").should('have.text', 'Oops! CPF inválido')
+    });
+
+
 });
