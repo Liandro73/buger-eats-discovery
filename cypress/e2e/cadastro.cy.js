f16961b146a57de544e4490a32c711e1153ffbaf
Dernival Liandro <liandro.sys@gmail.com>
2022-08-16 22:49:31 -0300

    Export default new page

--- cypress/e2e/cadastro.cy.js
+++ cypress/e2e/cadastro.cy.js
@@ -1,22 +1,6 @@
-import SignupPage from '../pages/SignupPage'
+import signup from '../pages/SignupPage'
 
 describe('Cadastro', () => {
-
-    before(function() {
-        cy.log('Tudo aqui é executado uma única vez ANTES de TODOS os casos de testes')
-    })
-
-    beforeEach(function() {
-        cy.log('Tudo aqui é executado sempre ANTES de CADA caso de teste')
-    })
-
-    after(function() {
-        cy.log('Tudo aqui é executado uma única vez DEPOIS de TODOS os casos de testes')
-    })
-
-    afterEach(function() {
-        cy.log('Tudo aqui é executado sempre DEPOIS de CADA caso de teste')
-    })
 
     it('Usuário deve se tornar um entregador', () => {
 
@@ -38,9 +22,7 @@ describe('Cadastro', () => {
         }
 
         const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
-
-        var signup = new SignupPage()
-
+        
         signup.go()
         signup.fillForm(deliver)
         signup.submit()
@@ -73,8 +55,6 @@ describe('Cadastro', () => {
         }
 
         const expectedMessage = 'Oops! CPF inválido'
-
-        var signup = new SignupPage()
 
         signup.go()
         signup.fillForm(deliver)
