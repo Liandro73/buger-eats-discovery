e5383d2eb10c6c6aaf49470cf9187547f07e3bf7
Dernival Liandro <liandro.sys@gmail.com>
2022-08-16 22:41:53 -0300

    Hooks

--- cypress/e2e/cadastro.cy.js
+++ cypress/e2e/cadastro.cy.js
@@ -1,6 +1,23 @@
 import SignupPage from '../pages/SignupPage'
 
 describe('Cadastro', () => {
+
+    before(function() {
+        cy.log('Tudo aqui é executado uma única vez ANTES de TODOS os casos de testes')
+    })
+
+    beforeEach(function() {
+        cy.log('Tudo aqui é executado sempre ANTES de CADA caso de teste')
+    })
+
+    after(function() {
+        cy.log('Tudo aqui é executado uma única vez DEPOIS de TODOS os casos de testes')
+    })
+
+    afterEach(function() {
+        cy.log('Tudo aqui é executado sempre DEPOIS de CADA caso de teste')
+    })
+
     it('Usuário deve se tornar um entregador', () => {
 
         var deliver = {
