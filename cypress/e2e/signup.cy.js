86d1238b5daf705125485af2cea5980946c718ec
Dernival Liandro <liandro.sys@gmail.com>
2022-08-18 23:31:08 -0300

    Factory e Faker com dados e CPF dinâmicos

--- cypress/e2e/signup.cy.js
+++ cypress/e2e/signup.cy.js
@@ -1,19 +1,22 @@
 import signup from '../pages/SignupPage'
+import signupFactory from '../factories/SignupFactory'
 
 describe('Signup', () => {
 
-    beforeEach(function () {
-        cy.fixture('deliver').then((d) => {
-            this.deliver = d
-        })
-    })
+    // beforeEach(function () {
+    //     cy.fixture('deliver').then((d) => {
+    //         this.deliver = d
+    //     })
+    // })
 
     it('User should be deliver', function () {
+
+        var deliver = signupFactory.deliver()
 
         const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
 
         signup.go()
-        signup.fillForm(this.deliver.signup)
+        signup.fillForm(deliver)
         signup.submit()
         signup.modalContentShouldBe(expectedMessage)
 
@@ -21,10 +24,13 @@ describe('Signup', () => {
 
     it('Incorrect document', function () {
 
+        var deliver = signupFactory.deliver()
+        deliver.cpf = 'X00000141AA'
+
         const expectedMessage = 'Oops! CPF inválido'
 
         signup.go()
-        signup.fillForm(this.deliver.cpf_invalido)
+        signup.fillForm(deliver)
         signup.submit()
         signup.alertMessageShouldBe(expectedMessage)
 
@@ -32,10 +38,13 @@ describe('Signup', () => {
 
     it('Incorrect email', function () {
 
+        var deliver = signupFactory.deliver()
+        deliver.email = 'dernival.teste.com'
+
         const expectedMessage = 'Oops! Email com formato inválido.'
 
         signup.go()
-        signup.fillForm(this.deliver.email_invalido)
+        signup.fillForm(deliver)
         signup.submit()
         signup.alertMessageShouldBe(expectedMessage)
 
